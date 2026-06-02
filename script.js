abeb82b81a855ad9f2cb9ff5f3cf0b691a58950a
jaishree2006 <jaishree2006happy@gmail.com>
2026-06-02 13:02:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,52 +5,46 @@ screen.classList.add('hidden');
 });
 
 document.getElementById('screen'+num).classList.remove('hidden');
-
 }
 
 function wrongAnswer(){
-document.getElementById('wrong').innerHTML=
+document.getElementById("wrong").innerHTML =
 "❌ Nice try Baby Boi 😂❤️";
 }
 
-let photos=[
+let photos = [
 "photo1.jpg",
 "photo2.jpg",
 "photo3.jpg",
 "photo4.jpg"
 ];
 
-let current=0;
+let current = 0;
 
 function nextPhoto(){
 
 current++;
 
-if(current>=photos.length){
-current=0;
+if(current >= photos.length){
+current = 0;
 }
 
-document.getElementById("slideshow").src=
-photos[current];
-
+document.getElementById("slideshow").src = photos[current];
 }
 
 function prevPhoto(){
 
 current--;
 
-if(current<0){
-current=photos.length-1;
+if(current < 0){
+current = photos.length - 1;
 }
 
-document.getElementById("slideshow").src=
-photos[current];
-
+document.getElementById("slideshow").src = photos[current];
 }
 
 function birthdayWish(){
 
-document.getElementById("wish").innerHTML=
+document.getElementById("wish").innerHTML =
 "🎉 HAPPY 20TH BIRTHDAY ASHIII ❤️<br><br>Love You Forever Baby Boi 🫶✨";
-
 }
