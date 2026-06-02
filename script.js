cfc396a74c23f450f048963280fe735afd9a2a31
jaishree2006 <jaishree2006happy@gmail.com>
2026-06-02 13:00:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
+function nextScreen(num){
 
+document.querySelectorAll('.screen').forEach(screen=>{
+screen.classList.add('hidden');
+});
+
+document.getElementById('screen'+num).classList.remove('hidden');
+
+}
+
+function wrongAnswer(){
+document.getElementById('wrong').innerHTML=
+"❌ Nice try Baby Boi 😂❤️";
+}
+
+let photos=[
+"photo1.jpg",
+"photo2.jpg",
+"photo3.jpg",
+"photo4.jpg"
+];
+
+let current=0;
+
+function nextPhoto(){
+
+current++;
+
+if(current>=photos.length){
+current=0;
+}
+
+document.getElementById("slideshow").src=
+photos[current];
+
+}
+
+function prevPhoto(){
+
+current--;
+
+if(current<0){
+current=photos.length-1;
+}
+
+document.getElementById("slideshow").src=
+photos[current];
+
+}
+
+function birthdayWish(){
+
+document.getElementById("wish").innerHTML=
+"🎉 HAPPY 20TH BIRTHDAY ASHIII ❤️<br><br>Love You Forever Baby Boi 🫶✨";
+
+}
