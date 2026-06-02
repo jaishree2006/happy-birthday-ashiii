8f73701ef0775907a891373a211bf7fed7e3d4a0
jaishree2006 <jaishree2006happy@gmail.com>
2026-06-02 13:06:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,23 @@ function birthdayWish(){
 document.getElementById("wish").innerHTML =
 "🎉 HAPPY 20TH BIRTHDAY ASHIII ❤️<br><br>Love You Forever Baby Boi 🫶✨";
 }
+setInterval(() => {
+
+let heart = document.createElement("div");
+
+heart.className = "heart";
+
+heart.innerHTML = "❤️";
+
+heart.style.left = Math.random() * 100 + "vw";
+
+heart.style.fontSize =
+(Math.random() * 20 + 15) + "px";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},6000);
+
+},500);
